cccb28e50f53d29b81c135d152f0317d09b6a5d4
ギッチ <34914112+Gitschi@users.noreply.github.com>
2018-07-19 23:53:46 +0900

    Add env.js file
    - Added file that takes the API keys from environment variables and puts them into an object

---
+++ env.js
@@ -0,0 +1,9 @@
+module.exports = {
+  keys: {
+      consumer_key: process.env.FGCBOT_CONSUMER_KEY,
+      consumer_secret: process.env.FGCBOT_CONSUMER_SECRET,
+      access_token: process.env.FGCBOT_ACCESS_TOKEN,
+      access_token_secret: process.env.FGCBOT_ACCESS_TOKEN_SECRET,
+      timeout_ms: 60*1000
+  }
+};
